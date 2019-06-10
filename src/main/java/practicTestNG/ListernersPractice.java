452892f3c402f83fa7ebeb9fd5291e2c36579eaf
Neel Savla <neelsavla@Neels-MacBook-Pro.local>
2019-06-10 11:06:27 -0700

    this is my second commit for may 2019

---
+++ src/main/java/practicTestNG/ListernersPractice.java
@@ -0,0 +1,11 @@
+package practicTestNG;
+
+public class ListernersPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi i am in Listerners");
+
+	}
+
+}
